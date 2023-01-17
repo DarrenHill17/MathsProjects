535572ce1672e3a16da9da5860983bcfc45edbff
DarrenHill17 <71755528+DarrenHill17@users.noreply.github.com>
2023-01-17 19:51:58 +0000

    Fixed input and added validation checks

--- Converters/Base N/base_n_converter.py
+++ Converters/Base N/base_n_converter.py
@@ -2,8 +2,12 @@ import numpy
 
 def convert_bases(input_string: str, input_base, output_base) -> str:
     '''Convert base n to'''
-    # Convert to base 10
+    # Validate input
     input_numbers = input_string.split('_')
+    if float(max(input_numbers)) >= input_base:
+        return 'Invalid input. A number in the input is larger than the input base.'
+
+    # Convert to base 10
     exponent = 0
     base10 = 0
     for digit in input_numbers[::-1]:
@@ -24,4 +28,4 @@ def convert_bases(input_string: str, input_base, output_base) -> str:
     return f'{input_string} in base {input_base} converts to {output} in base {output_base}'
 
 # Output
-print(convert_bases('1_0_2_2', numpy.e, numpy.pi))
+print(convert_bases('1_0_2_3', numpy.e, numpy.pi))
