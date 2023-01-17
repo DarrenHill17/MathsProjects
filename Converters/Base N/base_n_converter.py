9e86690cdd901d64ddc06f86e8be2938ce3a0d02
DarrenHill17 <71755528+DarrenHill17@users.noreply.github.com>
2023-01-17 19:50:05 +0000

    Fixed input

--- Converters/Base N/base_n_converter.py
+++ Converters/Base N/base_n_converter.py
@@ -1,9 +1,12 @@
-def convert_bases(input_number: str, input_base, output_base) -> str:
+import numpy 
+
+def convert_bases(input_string: str, input_base, output_base) -> str:
     '''Convert base n to'''
     # Convert to base 10
+    input_numbers = input_string.split('_')
     exponent = 0
     base10 = 0
-    for digit in input_number[::-1]:
+    for digit in input_numbers[::-1]:
         base10 += int(digit) * (input_base ** exponent)
         exponent += 1
 
@@ -18,7 +21,7 @@ def convert_bases(input_number: str, input_base, output_base) -> str:
     output = f'{str(carry % output_base)}{output}'
 
     # Function return
-    return f'{input_number} in base {input_base} converts to {output} in base {output_base}'
+    return f'{input_string} in base {input_base} converts to {output} in base {output_base}'
 
 # Output
-print(convert_bases('4216', 7, 2))
+print(convert_bases('1_0_2_2', numpy.e, numpy.pi))
