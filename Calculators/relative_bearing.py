8d358620166724765e0d44008a8da68e428fabc5
DarrenHill17 <71755528+DarrenHill17@users.noreply.github.com>
2023-02-06 12:47:06 +0000

    Initial commit

---
+++ Calculators/relative_bearing.py
@@ -0,0 +1,39 @@
+def calculate_q_codes(compass_heading: float, magnetic_variation: float, magnetic_deviation: float, relative_bearing: float) -> None:
+    '''Calculate the Q Codes for a given compass heading and relative bearing, such as with an ADF'''
+    output = [0, 0, 0, 0] # [QDM, QDR, QUJ, QTE]
+
+    # Calculate magnetic heading by correcting for compass deviation
+    magnetic_heading = compass_heading + magnetic_deviation
+
+    # Calculate true heading by correction for variation at the aircraft's position
+    true_heading = compass_heading + magnetic_variation + magnetic_deviation
+
+    # QDM
+    output[0] = magnetic_heading + relative_bearing
+
+    # QDR
+    output[1] = magnetic_heading + relative_bearing + 180
+
+    # QUJ
+    output[2] = true_heading + relative_bearing
+    
+
+    # QTE
+    output[3] = true_heading + relative_bearing + 180
+
+    # Normalise headings to be in range (0, 360]
+    for i in range(len(output)):
+        if output[i] <= 0:
+            while output[i] <= 0:
+                output[i] += 360
+        elif output[i] > 360:
+            while output[i] > 360:
+                output[i] -= 360
+
+    qdm = str(output[0]).rjust(3, '0') + '°'
+    qdr = str(output[1]).rjust(3, '0') + '°'
+    quj = str(output[2]).rjust(3, '0') + '°'
+    qte = str(output[3]).rjust(3, '0') + '°'
+    print(f'QDM: {qdm}\nQDR: {qdr}\nQUJ: {quj}\nQTE: {qte}')
+
+calculate_q_codes(310, -15, 0, 290)
