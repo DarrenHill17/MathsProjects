a5c0907ea343b9c195eca0053db070a0885ded11
DarrenHill17 <71755528+DarrenHill17@users.noreply.github.com>
2023-02-14 12:48:09 +0000

    Initial commit

---
+++ Calculators/raffle_tester.py
@@ -0,0 +1,9 @@
+from raffle_probability import RaffleProbability as R
+
+raffle = R()
+#raffle.test_strategies(6, 4,5,6)
+#raffle.test_strategies(6, 4,5,6,7,7,6,6)
+raffle.test_strategies(4, 2,3,2)
+# raffle.test_strategies(16, 14,15,16)
+# raffle.test_strategies(17, 11,3,19)
+# raffle.test_strategies(100, 20,30,40)
