6da36f7d1096c6e76c82c45052b3e1bd9a22a9ed
DarrenHill17 <71755528+DarrenHill17@users.noreply.github.com>
2023-02-23 06:38:09 +0000

    New file path, fixed return type

--- Calculators/Aviation/relative_bearing.py
+++ Calculators/Aviation/relative_bearing.py
@@ -1,4 +1,4 @@
-def calculate_q_codes(compass_heading: float, magnetic_variation: float, magnetic_deviation: float, relative_bearing: float) -> None:
+def calculate_q_codes(compass_heading: float, magnetic_variation: float, magnetic_deviation: float, relative_bearing: float) -> tuple:
     '''Calculate the Q Codes for a given compass heading and relative bearing, such as with an ADF'''
     output = [0, 0, 0, 0] # [QDM, QDR, QUJ, QTE]
 
@@ -16,7 +16,6 @@ def calculate_q_codes(compass_heading: float, magnetic_variation: float, magneti
 
     # QUJ
     output[2] = true_heading + relative_bearing
-    
 
     # QTE
     output[3] = true_heading + relative_bearing + 180
@@ -29,11 +28,14 @@ def calculate_q_codes(compass_heading: float, magnetic_variation: float, magneti
         elif output[i] > 360:
             while output[i] > 360:
                 output[i] -= 360
-
+    
+    # String output to console
     qdm = str(output[0]).rjust(3, '0') + '°'
     qdr = str(output[1]).rjust(3, '0') + '°'
     quj = str(output[2]).rjust(3, '0') + '°'
     qte = str(output[3]).rjust(3, '0') + '°'
     print(f'QDM: {qdm}\nQDR: {qdr}\nQUJ: {quj}\nQTE: {qte}')
+
+    return (qdm, qdr, quj, qte)
 
 calculate_q_codes(310, -15, 0, 290)
